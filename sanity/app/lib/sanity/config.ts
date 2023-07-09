6a8489c643a353ecc694d26afa7cca9798763bf1
matthew <99541432+matthewkyleherren@users.noreply.github.com>
2023-07-09 03:31:23 +0000

    Update config.tsujz

--- sanity/app/lib/sanity/config.ts
+++ sanity/app/lib/sanity/config.ts
@@ -2,6 +2,6 @@ export const config = {
   apiVersion: "2021-03-25",
   // Find these in your ./studio/sanity.json file
   dataset: "production",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "tyz898iu",
   useCdn: false,
 };
